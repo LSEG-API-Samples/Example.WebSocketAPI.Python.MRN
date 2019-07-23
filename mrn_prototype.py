d384c4a369b9074b723c1c0aef6fbb3d8734d7b5
Wasin Waeosri <wasin.waeosri@thomsonreuters.com>
2019-07-23 15:03:30 +0700

    change logs: handle multiple fragments

--- mrn_prototype.py
+++ mrn_prototype.py
@@ -64,37 +64,75 @@ def processRefresh(ws, message_json):
     decodeFieldList(message_json["Fields"])
 
 
+def parseNewsData(fragment):
+    decompressed_data = zlib.decompress(fragment, zlib.MAX_WBITS | 32)
+    print("News = %s" % decompressed_data)
+
+
 def processUpdate(ws, message_json):
-    #print("RECEIVED: Update Message")
-    #print(json.dumps(message_json, sort_keys=True, indent=2, separators=(',', ':')))
+    print("RECEIVED: Update Message")
+    # print(message_json)
 
     fields_data = message_json["Fields"]
     # Dump the FieldList first (for informational purposes)
     decodeFieldList(message_json["Fields"])
 
-    # Get data for all requried fields
-    fragment = fields_data["FRAGMENT"]
-    frag_num = fields_data["FRAG_NUM"]
-    guid = fields_data["GUID"]
-    mrn_src = fields_data["MRN_SRC"]
-    tot_size = int(fields_data["TOT_SIZE"])
     try:
-        fragment_decoded = base64.b64decode(fragment)
-        print("GUID  = %s" % guid)
-        print("TOT_SIZE = %d" % tot_size)
-        print("fragment length = %d" % len(fragment_decoded))
-        # if frag_num > 1:  # We are now processing more than one part of an envelope - retrieve the current details
+        # Get data for all requried fields
+        fragment = base64.b64decode(fields_data["FRAGMENT"])
+        frag_num = int(fields_data["FRAG_NUM"])
+        guid = fields_data["GUID"]
+        mrn_src = fields_data["MRN_SRC"]
 
-        if tot_size == len(fragment_decoded):  # Completed News
-            decompressed_data = zlib.decompress(
-                fragment_decoded, zlib.MAX_WBITS | 32)
-            print("News = %s" % decompressed_data)
-        else:
-            print("Multiple Fragments!!")
+        print("GUID  = %s" % guid)
+        print("FRAG_NUM = %d" % frag_num)
+        print("MRN_SRC = %s" % mrn_src)
+
+        #fragment_decoded = base64.b64decode(fragment)
+        print("fragment length = %d" % len(fragment))
+        if frag_num > 1:  # We are now processing more than one part of an envelope - retrieve the current details
+            guid_index = next((index for (index, d) in enumerate(
+                _news_envelopes) if d["guid"] == guid), None)
+            envelop = _news_envelopes[guid_index]
+            if envelop:
+                print("process multiple fragments for guid %s" %
+                      envelop["guid"])
+                # print(envelop)
+                #print("fragment before merge = %d" % len(envelop["data"]["fragment"]))
+
+                # Merge incoming fragment to current fragment
+                envelop["data"]["fragment"] = envelop["data"]["fragment"] + fragment
+
+                #print("TOT_SIZE from envelop = %d" % envelop["data"]["tot_size"])
+                #print("fragment after merge = %d" % len(envelop["data"]["fragment"]))
+                if envelop["data"]["tot_size"] == len(envelop["data"]["fragment"]):
+                    parseNewsData(envelop["data"]["fragment"])
+                else:
+                    return None
+        else:  # FRAG_NUM:1 The first fragment
+            tot_size = int(fields_data["TOT_SIZE"])
+            print("TOT_SIZE = %d" % tot_size)
+            if tot_size == len(fragment):  # Completed News
+                parseNewsData(fragment)
+                pass
+            else:
+                #print("Receiving Multiple Fragments!!")
+                print("Add new fragments to news envelop for guid %s" % guid)
+                _news_envelopes.append({
+                    "guid": guid,
+                    "data": {
+                        "fragment": fragment,
+                        "mrn_src": mrn_src,
+                        "frag_num": frag_num,
+                        "tot_size": tot_size
+                    }
+                })
+    except KeyError as keyerror:
+        print('KeyError exception: ', keyerror)
     except zlib.error as error:
-        print(error)
-    except:
-        print("Error!!!")
+        print('zlib exception: ', error)
+    except Exception as e:
+        print('exception: ', sys.exc_info()[0])
 
 
 def processStatus(ws, message_json):
@@ -249,6 +287,6 @@ if __name__ == "__main__":
 
     try:
         while True:
-            time.sleep(1)
+            time.sleep(600)
     except KeyboardInterrupt:
         web_socket_app.close()
